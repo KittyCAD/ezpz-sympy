fb6cb9657eb63d14797d1da30092e23d8ab8c210
Adam Chalmers <adam.chalmers@zoo.dev>
2025-12-18 20:03:39 -0600

    PointArcCoincident

--- main.py
+++ main.py
@@ -2,8 +2,80 @@ import sympy as sp
 
 
 def main():
-    horizontal_point_line_dist()
+    point_arc_coincident_ang()
+    # horizontal_point_line_dist()
     # vertical_point_line_dist()
+
+
+def cross2(u, v):
+    """Return the scalar z-component of the 2D cross product."""
+    return u[0] * v[1] - u[1] * v[0]
+
+
+def point_arc_coincident_ang():
+    # The arc (a = start, b = end, counterclockwise from A to B)
+    c = sp.Matrix(sp.symbols("cx cy", real=True))
+    a = sp.Matrix(sp.symbols("ax ay", real=True))
+    b = sp.Matrix(sp.symbols("bx by", real=True))
+
+    p = sp.Matrix(sp.symbols("px py", real=True))
+    px = p[0]
+    py = p[1]
+    ax = a[0]
+    ay = a[1]
+    bx = b[0]
+    by = b[1]
+    cx = c[0]
+    cy = c[1]
+
+    ua = a - c
+    ub = b - c
+    up = p - c
+
+    res1 = sp.Min(0, cross2(ua, up))
+    res2 = sp.Min(0, cross2(up, ub))
+
+    r1dpx = sp.rust_code(sp.simplify(res1.diff(px)))
+    r1dpy = sp.rust_code(sp.simplify(res1.diff(py)))
+    r1dax = sp.rust_code(sp.simplify(res1.diff(ax)))
+    r1day = sp.rust_code(sp.simplify(res1.diff(ay)))
+    r1dbx = sp.rust_code(sp.simplify(res1.diff(bx)))
+    r1dby = sp.rust_code(sp.simplify(res1.diff(by)))
+    r1dcx = sp.rust_code(sp.simplify(res1.diff(cx)))
+    r1dcy = sp.rust_code(sp.simplify(res1.diff(cy)))
+    print(f"let residual1 = {sp.rust_code(res1)}")
+    print(f"""// Partial derivatives, res1
+let r1dpx = {r1dpx};
+let r1dpy = {r1dpy};
+let r1dax = {r1dax};
+let r1day = {r1day};
+let r1dbx = {r1dbx};
+let r1dby = {r1dby};
+let r1dcx = {r1dcx};
+let r1dcy = {r1dcy};
+""")
+    print("\n\n\n\n\n")
+    r2dpx = sp.rust_code(sp.simplify(res2.diff(px)))
+    r2dpy = sp.rust_code(sp.simplify(res2.diff(py)))
+    r2dax = sp.rust_code(sp.simplify(res2.diff(ax)))
+    r2day = sp.rust_code(sp.simplify(res2.diff(ay)))
+    r2dbx = sp.rust_code(sp.simplify(res2.diff(bx)))
+    r2dby = sp.rust_code(sp.simplify(res2.diff(by)))
+    r2dcx = sp.rust_code(sp.simplify(res2.diff(cx)))
+    r2dcy = sp.rust_code(sp.simplify(res2.diff(cy)))
+    print(f"let residual2 = {sp.rust_code(res2)}")
+    print(f"""// Partial derivatives, res2
+let r2dpx = {r2dpx};
+let r2dpy = {r2dpy};
+let _r2dax = {r2dax};
+let _r2day = {r2day};
+let r2dbx = {r2dbx};
+let r2dby = {r2dby};
+let r2dcx = {r2dcx};
+let r2dcy = {r2dcy};
+""")
+
+    # print("residual 2: ", sp.rust_code(res2))
 
 
 def horizontal_point_line_dist():
